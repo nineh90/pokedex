6ecc6bf38554a89a71bd10372b777edc4302fcab
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-07-10 14:57:33 +0200

    first JS and styling

--- script.js
+++ script.js
@@ -19,11 +19,38 @@ function renderPokemonInfo(currentPokemon){
     let currentPokemonName = currentPokemon['species']['name']; 
     createPokemonImage(currentPokemon);
     createPokemnonHTML(currentCard, currentPokemonName);
+    renderPokemenAttack(currentPokemon);
+}
+
+function renderPokemenAttack(currentPokemon){
+    let attackOutput = document.getElementById('pokemonAttack');
+    let firstAttack = currentPokemon['moves'][0]['move']['name'];
+    let secondAttack = currentPokemon['moves'][1]['move']['name'];
+    let thirdAttack = currentPokemon['moves'][2]['move']['name'];
+
+    attackOutput.innerHTML += generateAttackHTML(firstAttack,secondAttack,thirdAttack);
+    console.log('Attacks are:',firstAttack,secondAttack,thirdAttack );
+
+}
+
+function generateAttackHTML(firstAttack,secondAttack,thirdAttack){
+    return`
+    <div class="text-center">
+        <div>
+            ${firstAttack}
+        </div>       
+        <div>       
+            ${secondAttack}
+        </div>    
+        <div>    
+            ${thirdAttack}
+        </div>  
+    </div>      
+`;
 }
 
 function createPokemnonHTML(currentCard, currentPokemonName){
     currentCard.innerHTML = '';
-
     currentCard.innerHTML += `
         <div>
             ${currentPokemonName}
@@ -33,9 +60,4 @@ function createPokemnonHTML(currentCard, currentPokemonName){
 function createPokemonImage(currentPokemon){
     let pokemonImage = currentPokemon['sprites']['front_shiny'];
     document.getElementById('pokemonImg').src = pokemonImage;
-    
-    
-    
-    //`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png`; 
-
 }
