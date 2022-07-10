79dabff1b713f3832d7978572f74287d2716038d
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-07-11 00:40:43 +0200

    styling current Pokemon/Load 100PokemonName from API

--- script.js
+++ script.js
@@ -2,35 +2,56 @@ let currentPokemon;
 let startSound = new Audio('');
 
 async function init(){
-    await loadPokemonFromApi();
+   await load100PokemonFromApi();
 }
 
+async function load100PokemonFromApi(){
+    let url = 'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0';
+    let responseUrl = await fetch(url);
+    let loadedPokemon = await responseUrl.json();
+    let pokemon = loadedPokemon['results']
+    forLoopRenderPokemon(pokemon);
+    console.log(pokemon);
+}
 
-async function loadPokemonFromApi(){
+function forLoopRenderPokemon(pokemon){
+    let loadedPokemon = document.getElementById('pokedexContainer');
+    loadedPokemon.innerHTML = '';
+    for (let i = 0; i < 100; i++) {
+        const pokeName = pokemon[i]['name'];
+        let name = pokeName.toUpperCase();
+        loadedPokemon.innerHTML += `
+        <div class="pokemon-name-container">${name}</div>`;
+        console.log(pokeName)
+    }
+}
+
+async function loadCurrentPokemonFromApi(){
     let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
     let responseUrl = await fetch(url);
-    let currentPokemon = await responseUrl.json();
+    currentPokemon = await responseUrl.json();
     console.log('Loaded Pokemon', currentPokemon);
     renderPokemonInfo(currentPokemon);
 }
 
-function renderPokemonInfo(currentPokemon){
+function renderPokemonInfo(){
     let currentCard = document.getElementById('pokemonName');
-    let currentPokemonName = currentPokemon['species']['name']; 
+    currentPokemonName = currentPokemon['species']['name']; 
     createPokemonImage(currentPokemon);
     createPokemnonHTML(currentCard, currentPokemonName);
     renderPokemenAttack(currentPokemon);
 }
 
-function renderPokemenAttack(currentPokemon){
+function renderPokemenAttack(){
     let attackOutput = document.getElementById('pokemonAttack');
     let firstAttack = currentPokemon['moves'][0]['move']['name'];
     let secondAttack = currentPokemon['moves'][1]['move']['name'];
     let thirdAttack = currentPokemon['moves'][2]['move']['name'];
+    
+    document.getElementById('attackLength').innerHTML = currentPokemon['moves'].length
 
     attackOutput.innerHTML += generateAttackHTML(firstAttack,secondAttack,thirdAttack);
     console.log('Attacks are:',firstAttack,secondAttack,thirdAttack );
-
 }
 
 function generateAttackHTML(firstAttack,secondAttack,thirdAttack){
@@ -50,14 +71,16 @@ function generateAttackHTML(firstAttack,secondAttack,thirdAttack){
 }
 
 function createPokemnonHTML(currentCard, currentPokemonName){
+    let name = currentPokemonName.toUpperCase(); 
+    currentCard.style.color = 'red'; 
     currentCard.innerHTML = '';
     currentCard.innerHTML += `
         <div>
-            ${currentPokemonName}
+            ${name}
         </div>`;
 }
 
-function createPokemonImage(currentPokemon){
+function createPokemonImage(){
     let pokemonImage = currentPokemon['sprites']['front_shiny'];
     document.getElementById('pokemonImg').src = pokemonImage;
 }
