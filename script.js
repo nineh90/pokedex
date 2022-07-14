23652f0903f36847dd20382e4f6d90dd45ec3ac2
nineh90 <102158158+nineh90@users.noreply.github.com>
2022-07-14 19:23:14 +0200

    styling current pokemon/Attack Image/ Attacks

--- script.js
+++ script.js
@@ -33,35 +33,134 @@ function generatePokemonHTML(name, i){
         <div id="pokemon${i}" onclick="showCurrentPokemonInfo('${name.toLowerCase()}')" class="pokemon-name-container">
             <div id="name${i}" class="margin-bottom">${name}</div>
             <div class="overview">
-                <img id="image${i}">
+                <img class="pokemon-image" id="image${i}">
             </div>
         </div>`
 }
 
-async function showCurrentPokemonInfo(pokeName){
+async function showCurrentPokemonInfo(pokeName, i){
     let url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     let responseUrl = await fetch(url);
     currentPokemon = await responseUrl.json();
     let currentPokemonCard = currentPokemon['abilities'];
-
-    for (let j = 0; j < currentPokemonCard.length; j++) {
-        const element = currentPokemonCard[j];
-        let currentAttack = currentPokemonCard[j]['ability']['name']
-        console.log('TEST',currentAttack)
-    }
-
+    let currentPokemonClass = currentPokemon['types'][0]['type']['name'];
+    setBackgroundCurrentPokemon(currentPokemonClass);
+    forLoopCurrentCard(currentPokemonCard);
     setStyleAttributes(pokeName);
     let image = document.getElementById(`activePokemonImage`);
     generateImage(image);
-    console.log(pokeName, currentPokemonCard )
+    console.log(pokeName, currentPokemonClass)
+}
+
+function setBackgroundCurrentPokemon(currentPokemonClass){
+    let imageBackground = document.getElementById('activePokemonImage');
+        currentPokemonIsGrass(currentPokemonClass, imageBackground);
+        currentPokemonIsWater(currentPokemonClass, imageBackground);
+        currentPokemonIsFire(currentPokemonClass, imageBackground);
+        currentPokemonIsBug(currentPokemonClass, imageBackground);
+        currentPokemonIsNormal(currentPokemonClass, imageBackground);
+        currentPokemonIsPoison(currentPokemonClass, imageBackground);
+        currentPokemonIsElectric(currentPokemonClass, imageBackground);
+        currentPokemonIsGround(currentPokemonClass, imageBackground);
+        currentPokemonIsFairy(currentPokemonClass, imageBackground);
+        currentPokemonIsRock(currentPokemonClass, imageBackground);
+
+}
+
+function forLoopCurrentCard(currentPokemonCard){
+    let attackOutput = document.getElementById('attack');
+    attackOutput.innerHTML = ''; 
+        for (let j = 0; j < currentPokemonCard.length; j++) {
+            const element = currentPokemonCard[j];
+            let currentAttack = currentPokemonCard[j]['ability']['name'];
+            attackOutput.innerHTML += `<li>${currentAttack}</li>`;
+            console.log('TEST',currentAttack);
+        }
 }
 
 function setStyleAttributes(pokeName){
     document.getElementById('currentCard').classList.remove('d-none');
     document.getElementById('pokedexContainer').classList.add('d-none');
     document.getElementById('info').style.backgroundImage =  "url('./img/pikachu2.jpg')";
-    document.getElementById('activePokemonName').innerHTML = 'Deine Mudda ist<br>' + pokeName.toUpperCase();
+    document.getElementById('activePokemonName').innerHTML = pokeName.toUpperCase();
 }
+
+function currentPokemonIsGrass(currentPokemonClass, imageBackground){
+    if (currentPokemonClass === 'grass') {
+        imageBackground.style.backgroundColor  = 'green';
+        document.getElementById('activePokemonClass').src = './img/grass.png';
+    } 
+}
+
+function currentPokemonIsWater(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'water' ){
+        imageBackground.style.backgroundColor  = 'blue';
+        document.getElementById('activePokemonClass').src = './img/water.png';
+    }  
+}
+
+function currentPokemonIsFire(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'fire'){
+        imageBackground.style.backgroundColor  = 'orangered';
+        document.getElementById('activePokemonClass').src = './img/flames.png';
+    } 
+}
+
+function currentPokemonIsBug(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'bug'){
+        imageBackground.style.backgroundColor  = 'lightgray';
+        document.getElementById('activePokemonClass').src = './img/antivirus.png';
+    } 
+}
+
+function currentPokemonIsNormal(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'normal'){
+        imageBackground.style.backgroundColor  = 'lightblue';
+        document.getElementById('activePokemonClass').src = './img/rating.png';
+    } 
+}
+
+function currentPokemonIsPoison(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'poison'){
+        imageBackground.style.backgroundColor  = 'darkred';
+        document.getElementById('activePokemonClass').src = './img/poison.png';
+
+    }
+}
+
+function currentPokemonIsElectric(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'electric'){
+        imageBackground.style.backgroundColor  = 'darkblue';
+        document.getElementById('activePokemonClass').src = './img/lightning.png';
+
+    }   
+}
+
+function currentPokemonIsGround(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'ground'){
+        imageBackground.style.backgroundColor  = 'lightgreen';
+        document.getElementById('activePokemonClass').src = './img/ground.png';
+    } 
+}
+
+function currentPokemonIsFairy(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'fairy'){
+        imageBackground.style.backgroundColor  = 'lightyellow';
+        document.getElementById('activePokemonClass').src = './img/magic-wand.png';
+
+    }
+}
+function currentPokemonIsRock(currentPokemonClass, imageBackground){
+    if(currentPokemonClass === 'rock'){
+        imageBackground.style.backgroundColor  = 'grey';
+        document.getElementById('activePokemonClass').src = './img/rocks.png';
+    }
+}
+
+
+
+
+
 
 async function loadCurrentPokemonFromApi(pokeName,i){
     await getPokemonDataFromAPI(pokeName);
